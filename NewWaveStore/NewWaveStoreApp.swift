1f48e486e458f4723135e89ff9a8939e657300e8
Ella Speer-Zisook <s015721@students.lmsd.org>
2022-11-21 07:38:35 -0500

    Initial Commit

---
+++ NewWaveStore/NewWaveStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewWaveStoreApp.swift
+//  NewWaveStore
+//
+//  Created by Speer-Zisook, Ella on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct NewWaveStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
