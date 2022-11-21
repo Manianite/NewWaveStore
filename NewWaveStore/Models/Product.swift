52566a271529b965affff1a46165602aa1a856d0
Ella Speer-Zisook <s015721@students.lmsd.org>
2022-11-21 08:08:05 -0500

    added images and product class

---
+++ NewWaveStore/Models/Product.swift
@@ -0,0 +1,16 @@
+//
+//  Product.swift
+//  NewWaveStore
+//
+//  Created by Speer-Zisook, Ella on 11/21/22.
+//
+
+import Foundation
+
+class Product {
+    var name = ""
+    var description = ""
+    var picture = ""
+    var quantity = 1
+    var price = 0.0
+}
